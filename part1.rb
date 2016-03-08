34128aeaca60e9f3b35b71f0de67a445b00e4b74
Vivek Patel <vnp229@gmail.com>
2016-03-08 13:38:24 -0600

    changed part 1 to use regex

--- part1.rb
+++ part1.rb
@@ -1,8 +1,6 @@
 str = "Tacos must not have lettuce Only meat and cheese and salsa"
 
-arr = str.split(" ")
-word_length_5 = 0
-arr.each do |word|
-  if word.length == 5 then word_length_5 += 1 end
-end
-puts word_length_5
+arr = str.scan /[a-zA-Z]+\b/
+word_length_5 = str.scan /\b[a-zA-Z]{5}\b/
+print word_length_5
+puts
---
+++ part2.rb
@@ -0,0 +1,36 @@
+movies = []
+
+movies << {
+  title: "Forest Gump",
+  budget: 55,
+  stars: ["Tom Hanks"]
+}
+movies << {
+  title: "Star Wars",
+  budget: 11,
+  stars: ["Mark Hamill", "Harrison Ford"]
+}
+movies << {
+  title: "Batman Begins",
+  budget: 150,
+  stars: ["Christian Bale", "Liam Neeson", "Michael Caine"]
+}
+movies << {
+  title: "Titanic",
+  budget: 200,
+  stars: ["Kate Winslet", "Leonardo DiCaprio"]
+}
+movies << {
+  title: "Inception",
+  budget: 160,
+  stars: ["Leonardo DiCaprio", "JGL"]
+}
+
+low_budget = Array.new
+
+leonardo_movies = Array.new
+
+movies.each do |movie_hsh|
+  
+
+end
