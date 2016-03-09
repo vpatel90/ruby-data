9bb22af04eba1c2449c16b52d32797458fbc9add
Vivek Patel <vnp229@gmail.com>
2016-03-08 22:16:56 -0600

    added classes

--- part2.rb
+++ part2.rb
@@ -1,3 +1,37 @@
+class MoviesByActor
+  def initialize(movies, actor)
+    @movies = movies
+    @actor = actor
+    @movies_by_actor = []
+  end
+
+  def filter
+    @movies.each do |movie_hsh|
+      stars = movie_hsh[:stars]
+      stars.each do |star|
+        if star == @actor then @movies_by_actor.push(movie_hsh[:title]) end
+      end
+    end
+    return @movies_by_actor
+  end
+end
+
+class MoviesByBudget
+  def initialize(movies, budget)
+    @movies = movies
+    @budget = budget
+    @low_budget_flix = []
+  end
+
+  def filter_low_budget
+    @movies.each do |movie_hsh|
+      budget = movie_hsh[:budget]
+      if budget < @budget then @low_budget_flix.push(movie_hsh[:title]) end
+    end
+    return @low_budget_flix
+  end
+end
+
 movies = []
 
 movies << {
@@ -26,21 +60,9 @@ movies << {
   stars: ["Leonardo DiCaprio", "JGL"]
 }
 
-low_budget = Array.new
-
-leonardo_movies = Array.new
-
-movies.each do |movie_hsh|
-  budget = movie_hsh[:budget]
-  stars = movie_hsh[:stars]
-  if budget < 100 then low_budget.push(movie_hsh[:title]) end
-
-  stars.each do |star|
-    if star == "Leonardo DiCaprio" then leonardo_movies.push(movie_hsh[:title]) end
-  end
-
-end
-
-print low_budget
+leonardo_movies = MoviesByActor.new(movies, "Leonardo DiCaprio")
+print leonardo_movies.filter
 puts
-print leonardo_movies
+low_budget = MoviesByBudget.new(movies, 100)
+print low_budget.filter_low_budget
+puts
